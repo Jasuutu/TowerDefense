f62461780b6e3dd4a97e5f50f31e2e6f3191e07a
Jasuutu <cloudstrife.schultz@gmail.com>
2020-03-21 00:01:07 -0400

    added base sprite.

--- TowerDefenseGame/Assets/Scripts/PlayerBase.cs
+++ TowerDefenseGame/Assets/Scripts/PlayerBase.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerBase : MonoBehaviour
 {
+    [SerializeField] protected int Health { get; set; } = 100;
     // Start is called before the first frame update
     void Start()
     {
