c14d80c6a256e54d14bae8c15e2a4fc71f341e3f
Jasuutu <cloudstrife.schultz@gmail.com>
2020-03-21 20:19:32 -0400

    add playerbase and wip player base code

--- TowerDefenseGame/Assets/Scripts/PlayerBase.cs
+++ TowerDefenseGame/Assets/Scripts/PlayerBase.cs
@@ -1,19 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class PlayerBase : MonoBehaviour
 {
-    [SerializeField] protected int health  = 100;
+    private const string HealthStringStart = "";
+
+    [SerializeField] protected int maxHealth = 100;
+    [SerializeField] protected int health = 100;
+    [SerializeField] protected int defense = 0;
+    [SerializeField] protected Text healthText;
+
+    public int Health => health;
+    public int Defense => defense;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateHealthText();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void TakeDamage(int damageValue)
+    {
+        health = health - damageValue - defense;
+        UpdateHealthText();
+    }
+
+    public void UpdateHealthText()
+    {
+        healthText.text = $"{maxHealth} / {health}";
     }
 }
