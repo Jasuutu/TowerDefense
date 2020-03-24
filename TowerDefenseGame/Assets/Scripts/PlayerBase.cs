458abe82b2cc8866a6aaff0cc6d367ea029ea075
Jasuutu <cloudstrife.schultz@gmail.com>
2020-03-24 18:11:30 -0400

    Develop (#13)
    * Added Start and Finish positions to the level.
    
    * add playerbase and wip player base code
    
    * Added unit tests for playerbase logic.
    
    * Fixed issue with take damage formula and added a test for it.

--- TowerDefenseGame/Assets/Scripts/PlayerBase.cs
+++ TowerDefenseGame/Assets/Scripts/PlayerBase.cs
@@ -1,19 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerBase : MonoBehaviour
 {
-    [SerializeField] protected int health  = 100;    
+    [SerializeField] protected int maxHealth = 100;
+    [SerializeField] protected int health = 100;
+    [SerializeField] protected Text healthText;
+
+    public int Health => health;
+    public int Defense = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (healthText == null)
+        {
+            throw new ArgumentNullException(nameof(healthText));
+        }
+        UpdateHealthText();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void TakeDamage(int damageValue)
+    {
+        health = health - (damageValue - Defense);
+        UpdateHealthText();
+    }
+
+    public void UpdateHealthText()
+    {
+        healthText.text = $"{health} / {maxHealth}";
     }
 }
---
+++ TowerDefenseGame/Assets/Tests/PlayerBaseTests.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+
+namespace Tests
+{
+    public class PlayerBaseTests
+    {
+        private PlayerBase playerBase;
+        private Text healthText;
+
+        [SetUp]
+        public void Setup()
+        {
+            var prefab = AssetDatabase.LoadAssetAtPath("Assets/Prefabs/PlayerBase.prefab", typeof(PlayerBase));
+            var playerBaseGameObject = MonoBehaviour.Instantiate(prefab) as PlayerBase;
+            playerBase = playerBaseGameObject.GetComponent<PlayerBase>();
+            healthText = playerBase.GetComponentInChildren<Text>();
+        }
+
+        [UnityTest]
+        public IEnumerator PlayerBaseLosesHealthWhenTakingDamage()
+        {
+            Assert.AreEqual(playerBase.Health, 100);
+
+            playerBase.TakeDamage(10);
+
+            Assert.AreEqual(playerBase.Health, 90);
+
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator PlayBaseHeathTextShouldReflectCurrentTotal()
+        {
+            Assert.AreEqual(healthText.text, "100 / 100");
+
+            playerBase.TakeDamage(10);
+
+            Assert.AreEqual(healthText.text, "90 / 100");
+
+            yield return new WaitForSeconds(.01f);
+        }
+
+        [UnityTest]
+        public IEnumerator PlayerBaseDefenseReducesDamage()
+        {
+            playerBase.Defense = 10;
+            playerBase.TakeDamage(20);
+
+            Assert.AreEqual(playerBase.Health, 90);
+            
+            yield return null;
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Object.Destroy(playerBase.gameObject);
+        }
+    }
+}
